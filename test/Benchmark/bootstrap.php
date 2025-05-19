7782b3743b3929379d1195b5c08ff5e5c99a7fc5
Daniel Hernandez <daniel.hernandez@xing.com>
2025-05-19 03:06:47 +0200

    test: improve benchmark testing
    test: improve benchmark testing
    
    benchmark: improve benchmark regression
    
    benchmark: improve benchmark regression tests
    
    benchmark: improve benchmark regression tests
    
    benchmark: improve benchmark regression tests
    
    benchmark: improve regression test

--- test/Benchmark/bootstrap.php
+++ test/Benchmark/bootstrap.php
@@ -4,8 +4,38 @@ declare(strict_types=1);
 
 require __DIR__.'/../../vendor/autoload.php';
 
-exec('php -f test/_data/createMeasurements.php 1000000000 test/_data/measurements.csv', $output, $code);
+$files = [
+    'oil-and-gas.parquet' => 'https://github.com/plotly/datasets/raw/refs/heads/master/oil-and-gas.parquet',
+    'dutch_railway_network.duckdb' => 'http://blobs.duckdb.org/data/dutch_railway_network.duckdb',
+];
 
-if (0 !== $code) {
-    throw new RuntimeException(implode("\n", $output));
+
+foreach ($files as $file => $url) {
+    if (file_exists($file)) {
+        return;
+    }
+
+    $ch = curl_init($url);
+
+    $fp = fopen($file, 'w+');
+
+    curl_setopt_array($ch, [
+        CURLOPT_FILE           => $fp,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_FAILONERROR    => true,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false
+    ]);
+
+    $success = curl_exec($ch);
+
+    fclose($fp);
+
+    if (!$success) {
+        $error = curl_error($ch);
+        curl_close($ch);
+        throw new RuntimeException("Error downloading {$url}: {$error}");
+    }
+
+    curl_close($ch);
 }
