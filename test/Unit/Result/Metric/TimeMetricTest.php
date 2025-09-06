0ef8475022e0699761ef714ed7e0f082553d8db2
Daniel Hernandez <daniel.hernandez@xing.com>
2025-09-06 08:53:51 +0200

    fix: fix division by zero when no metrics

---
+++ test/Unit/Result/Metric/TimeMetricTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit\Result\Metric;
+
+use PHPUnit\Framework\TestCase;
+use Saturio\DuckDB\Result\Metric\TimeMetric;
+
+class TimeMetricTest extends TestCase
+{
+    public function testEmptyMetrics(): void
+    {
+        $timeMetric = TimeMetric::create();
+        self::assertEquals(0, $timeMetric->getTotalMilliseconds());
+        self::assertEquals(0, $timeMetric->getPhpPercentage());
+        self::assertEquals(0, $timeMetric->getPhpMilliseconds());
+        self::assertEquals(100, $timeMetric->getNativePercentage());
+        self::assertEquals(0, $timeMetric->getNativeMilliseconds());
+    }
+
+    public function testFiftyFiftyPercentage(): void
+    {
+        $timeMetric = TimeMetric::create();
+        usleep(100);
+        $timeMetric->switch();
+        usleep(100);
+        $timeMetric->end();
+
+        self::assertEquals(50, $timeMetric->getPhpPercentage());
+        self::assertEquals(50, $timeMetric->getNativePercentage());
+    }
+
+    public function testAllPHPPercentage(): void
+    {
+        $timeMetric = TimeMetric::create();
+        usleep(100);
+        $timeMetric->end();
+
+        self::assertEquals(100, $timeMetric->getPhpPercentage());
+        self::assertEquals(0, $timeMetric->getNativePercentage());
+    }
+
+    public function testAllNativePercentage(): void
+    {
+        $timeMetric = TimeMetric::create();
+        $timeMetric->switch();
+        usleep(100);
+        $timeMetric->end();
+
+        self::assertEquals(0, $timeMetric->getPhpPercentage());
+        self::assertEquals(100, $timeMetric->getNativePercentage());
+    }
+}
