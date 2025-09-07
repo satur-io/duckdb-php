a54573b3e2baa60f3c6f4cc5f374f9fb327af711
asrar <aszenz@gmail.com>
2025-09-07 13:23:13 +0200

    feat: Expose get table names

---
+++ test/Integration/GetTableNamesTest.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Integration;
+
+use PHPUnit\Framework\TestCase;
+use Saturio\DuckDB\FFI\DuckDB as FFIDuckDB;
+use Saturio\DuckDB\DuckDB;
+
+class GetTableNamesTest extends TestCase
+{
+    private DuckDB $db;
+
+    protected function setUp(): void
+    {
+        $this->db = DuckDB::create();
+    }
+
+    public function testGetTableNames()
+    {
+        $query = 'SELECT * FROM my_table JOIN f ON f.id = my_table.id';
+        $tableNames = $this->db->getTableNames($query);
+        $this->assertSame(['f', 'my_table'], $tableNames);
+    }
+}
