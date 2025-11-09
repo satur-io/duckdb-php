b730b135289b923ea92a8a1eb696cc5a5d338b2b
Daniel Hernandez <daniel.hernandez@xing.com>
2025-11-09 17:03:14 +0100

    ci: fix benchmark regression tests

--- scripts/run_local_benchmark_regression.sh
+++ scripts/run_local_benchmark_regression.sh
@@ -3,6 +3,7 @@ export CURRENT_BRANCH=$(git branch --show-current)
 git switch main
 git checkout ${CURRENT_BRANCH} test/Benchmark phpbench-local.json # preload.php
 composer dump-autoload
+php -r "require './vendor/autoload.php'; Saturio\DuckDB\CLib\Installer::install();"
 vendor/bin/phpbench run --config=phpbench-local.json --tag=main --report=duckdb_benchmark_report
 git switch ${CURRENT_BRANCH}
 composer dump-autoload
