56f48f6b72829ecf92a8679997cb231997fe1b51
James Bell <james.bell@thedatacity.com>
2026-03-24 13:56:55 +0000

    core: update clibs to v1.5.1

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
-const DUCKDB_PHP_LIB_VERSION = '1.5.0';
+const DUCKDB_PHP_LIB_VERSION = '1.5.1';
 const DUCKDB_PHP_LIB_CHECKSUMS = [
-    'linux-amd64' => 'e9b2adb9dc5348da57eee9562782e0e7f543e74ff26d634bc7c208a5b6c59a1c',
-    'linux-arm64' => '5b76dfb100ff6d19c9bbe6add29226471353c8bdba469ebb562fd52c91bbb9ec',
-    'osx-universal' => '071ef2f775c5bef805fcb9e58504cc2024aee82e5227f2b6322d0bdef832130e',
-    'windows-amd64' => '691ee5285b04fe1accb17b05ef53d397aedaf6bc852661592f9e02219dc7d1cb',
-    'windows-arm64' => '223ad9e7e2654e512c33bf710a0472c5276bd1911b3af02292078d1c4370b75f',
+    'linux-amd64' => '21aec66a60eae1696270ba715a481ab066a88d99a62718d0577579ac1a7a4834',
+    'linux-arm64' => '203b58511255b37328d3c2673798552af84ea73a5b1e29e8e9e1813ffd3dd064',
+    'osx-universal' => 'd9dd723c59f8571202b468f6bf71d4555238544553dd1445e6c9ecb39f54c0f3',
+    'windows-amd64' => 'b85febb52a7b2e6d6891fcfffd8685ea974012f952cdc8dadef9c81e281d730d',
+    'windows-arm64' => 'faf0af74503f7037090b46cbb61fe0238c434670a5b883e5425ed1121110ae64',
 ];
